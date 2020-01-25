merothh/aospa_device_xiaomi_mido

mido: Add basic USB HAL that reports no status change

device.mk
@@ -349,7 +349,7 @@ PRODUCT_COPY_FILES += \
 
 # USB HAL
 PRODUCT_PACKAGES += \
-    android.hardware.usb@1.0-service.basic
+    android.hardware.usb@1.0-service.xiaomi_mido
 
 # Vibrator
 PRODUCT_PACKAGES += \

AliHasan7671/ten-android_device_xiaomi_mido

Change-Id: I38bfe869f9b1b66db0e13249f65e438878d06eff
Signed-off-by: Ali Hasan <ahb7671@gmail.com>

device.mk
@@ -351,7 +351,7 @@ PRODUCT_COPY_FILES += \
 
 # USB HAL
 PRODUCT_PACKAGES += \
-    android.hardware.usb@1.0-service.basic
+    android.hardware.usb@1.0-service.basic_mido
 
 # Vibrator
 PRODUCT_PACKAGES += \