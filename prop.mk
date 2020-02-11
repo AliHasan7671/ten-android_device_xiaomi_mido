2e4614a09db6e2bfa88306c97fe2b659cc6c9c11
Ali Hasan <ahb7671@gmail.com>
2020-02-11 20:50:18 +0500

    mido: disable privapp_permissions enforcement
    Signed-off-by: Ali Hasan <ahb7671@gmail.com>

--- prop.mk
+++ prop.mk
@@ -199,3 +199,7 @@ wifi.interface=wlan0
 # Wifi-Display
 PRODUCT_PROPERTY_OVERRIDES += \
 persist.sys.wfd.virtual=0
+
+# Misc
+PRODUCT_PROPERTY_OVERRIDES += \
+ro.control_privapp_permissions=disable
